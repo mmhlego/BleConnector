0dfa13d30c5774e3d536ac6b5034c27a467c0657
mmhlego <mmhlego@gmail.com>
2022-08-28 11:50:08 +0430

    Added command parameter validators

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BleConnector.Models;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 /// <summary>
 /// Program entry point
@@ -6,9 +9,42 @@
 namespace BleConnector {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Task.Run(async () => {
+                while (true) {
+                    /*
+                       *      Input format:
+                       *          <device type> <mac address>
+                       *      
+                       *      First we scan the environment for current device. If found, we'll connect and get its measurements according to <device type>
+                       */
+                    string[] commands = Console.ReadLine().Split(' ');
+                    // Check if the command is valid
+                    if (commands.Length != 2) {
+                        Console.WriteLine("Invalid command format. Format: <Device_Type> <Mac_Address>");
+                        continue;
+                    }
 
-            Console.ReadKey();
+                    // Check if device type is valid
+                    if (!Enum.TryParse(commands[0], out DeviceTypes deviceType)) {
+                        Console.WriteLine($"Invalid device type. Valid device types: {Enum.GetValues(typeof(DeviceTypes))}");
+                        continue;
+                    }
+
+                    // Check if mac address is valid
+                    if (!Regex.IsMatch(commands[1], "(.{2})(.{2})(.{2})(.{2})(.{2})(.{2})")) {
+                        Console.WriteLine("Invalid mac address format. Correct format: XX:XX:XX:XX:XX:XX");
+                        continue;
+                    }
+
+                    // TODO: Scan and connect to device
+
+                    // TODO: Check if connected
+
+                    // TODO: Get bytes and parse to measurements
+
+                    // TODO: Print data in json format
+                }
+            });
         }
     }
 }
