6aadca3eba5257c6a2159140a0fdc985ca8d6ba5
mmhlego <mmhlego@gmail.com>
2022-09-10 13:37:38 +0430

    Added Stethoscope measurement model

---
+++ Models/StethoscopeMeasurement.cs
@@ -0,0 +1,8 @@
+﻿namespace BleConnector.Models {
+    /// <summary>
+    /// This class represents the results model for 'Mintti stetho D2' stethoscope device
+    /// </summary>
+    class StethoscopeMeasurement {
+        public string FileName { get; set; }
+    }
+}
